755aeb2d44628e0bce25ba1dc570c52275ecbb1f
KoranitAnanpoonsap <koranit.bm@gmail.com>
2025-01-03 01:52:35 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// filepath: /d:/TFPA/cfp_client/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/financial-planner-cfp_client-part/',
-  build: {
-    sourcemap: false, // Disable source maps to prevent 'eval' usage
-    minify: 'esbuild', // Ensure using esbuild for minification (default)
-  },
-})
+  plugins: [react()],
+});
